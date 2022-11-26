8848415c2d8c77cdc8a4ec976eed969f28b34c58
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-11-27 00:36:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const cards=[]
+function writeCards(gifts,surprise){
+for (let i=0;i<gifts.length;i++){
+gifts[i]=`Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`;}
+return gifts;
+}
+console.log(writeCards(["Peter","Alice","Grace"],"surprise"));
+function countDown(number){
+while(number>=0){
+console.log(number--);
+debugger;
+}
+}
+
+countDown(4)
